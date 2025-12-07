17e80ff346f6b1ab6630ae92f49df41de3eb97c4
nixhex <fevinster@gmail.com>
2025-12-07 10:12:53 -0800

    Initial Commit

---
+++ VolterriaSK/VolterriaSKApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolterriaSKApp.swift
+//  VolterriaSK
+//
+//  Created by Devin R Cohen on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct VolterriaSKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
